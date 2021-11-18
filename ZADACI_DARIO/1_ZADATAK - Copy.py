c35477e57eec60ae5beafa0a0b6093d8c7617538
sashacole <ivana.studioritual@gmail.com>
2021-11-18 23:00:51 +0100

    new folder

---
+++ ZADACI_DARIO/1_ZADATAK - Copy.py
@@ -0,0 +1,31 @@
+#1. Kreirajte dvije brojčane varijable te ispišite rezultat operatora usporedbe (usporedba jednakosti, 
+#nejednakosti, strogo veće, strogo manje, veće ili jednako, manje ili jednako). 
+
+a=12
+b=10
+print(12==10)
+print(12!=10)
+print(12>10)
+print(12<10)
+print(12>=10)
+print(12<=10)
+
+
+
+
+
+
+
+
+
+
+
+         
+      
+                            
+
+
+
+
+
+
